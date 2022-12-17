14be3769ce7eead2191c7eb844642548c98b02df
portu22 <kuraytqm@gmail.com>
2022-12-17 23:50:33 +0100

    aa

--- js/main.js
+++ js/main.js
@@ -41,49 +41,49 @@ const cardObject = [
   },
   {
     id: 2,
-    title: "Budget App",
+    title: "San roque's festival",
     subTitle: "",
     imageMobile:
-      "https://user-images.githubusercontent.com/37457094/188992488-27c28402-4bc7-498c-9477-3f560060a2dd.png",
+      "https://prnt.sc/YHbAGw1DdpqG",
     imageDesktop:
-      "https://user-images.githubusercontent.com/37457094/188992488-27c28402-4bc7-498c-9477-3f560060a2dd.png",
-    details: ["Back End Dev", "2022"],
+      "https://prnt.sc/YHbAGw1DdpqG",
+    details: ["Exciting", "Happy", "Only VR"],
     description:
-      "This is a ruby on rails mobile web application where you can manage your budget: you have a list of transactions associated with a category, so that you can see how much money you spent and on what.",
-    langs: ["ruby", "ruby on rails", "css"],
-    gitLink: "https://budgetapp-1.herokuapp.com/",
-    srcLink: "https://github.com/ismailakinkunmi/rails-budget-app",
+      "texto de prueba.",
+    langs: ["Doraemon", "Copyright", "Reserve"],
+    gitLink: "https://www.youtube.com/watch?v=wohwc9MQt6A",
+    srcLink: "https://www.youtube.com/watch?v=wohwc9MQt6A",
   },
   {
     id: 3,
-    title: "Space Travlers' Hub",
+    title: "Torremanzana's municipal pool",
     subTitle: "",
     imageMobile:
-      "https://user-images.githubusercontent.com/37457094/189049737-69d1bb57-cd15-477a-af04-68771c9d7654.png",
+      "https://prnt.sc/u3x-ilIthLdB",
     imageDesktop:
-      "https://user-images.githubusercontent.com/37457094/189049737-69d1bb57-cd15-477a-af04-68771c9d7654.png",
-    details: ["Front End Dev", "2022"],
+      "https://prnt.sc/u3x-ilIthLdB",
+    details: ["Chill", "Relaxing","Multiplayer","Only VR"],
     description:
-      "Space Travelers' Hub is working with the real live data from the SpaceX API.",
-    langs: ["html", "css", "React", "Redux"],
-    gitLink: "#",
-    srcLink: "https://github.com/ismailakinkunmi/space-travelers-hub",
+      "texto de prueba.",
+    langs: ["Doraemon", "Copyright", "Reserve"],
+    gitLink: "https://www.youtube.com/watch?v=wohwc9MQt6A",
+    srcLink: "https://www.youtube.com/watch?v=wohwc9MQt6A",
   },
   {
     id: 4,
-    title: "World-pandemic: Covid-19",
+    title: "Rafting in Villaverde mojinas",
     subTitle: "",
     imageMobile:
-      "https://user-images.githubusercontent.com/37457094/170477032-5b19dd99-5ba1-422b-a486-405167302610.png",
+      "https://prnt.sc/-7RUZICKoMqU",
 
     imageDesktop:
-      "https://user-images.githubusercontent.com/37457094/170477032-5b19dd99-5ba1-422b-a486-405167302610.png",
-    details: ["Front End Dev", "2022"],
+      "https://prnt.sc/-7RUZICKoMqU",
+    details: ["Chill", "Exciting","Amazing", "Multiplayer", "VR And 1 Person"],
     description:
-      "The application allow users to check covid-19 daily updated cases from around the world.It is working with the real live data disease.sh - Open Disease Data API. ",
-    langs: ["html", "css", "react", "redux"],
-    gitLink: "https://covid19-world-panemic.herokuapp.com/",
-    srcLink: "https://github.com/ismailakinkunmi/covid-19-world-pandemic",
+      "texto de prueba.",
+    langs: ["Doraemon", "Copyright", "Reserve"],
+    gitLink: "https://www.youtube.com/watch?v=wohwc9MQt6A",
+    srcLink: "https://www.youtube.com/watch?v=wohwc9MQt6A",
   },
 ];
 
